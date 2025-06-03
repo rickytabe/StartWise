2a4b22a1bc2308d03d3756e049cad98e8b56a695
ricky tabe <rickytabe2@gamil.com>
2025-06-03 16:25:29 +0100

    Corrected some tailwind issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-//import tailwind from 'tailwindcss'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),],
-  assetsInclude: ['**/*.worker.mjs'],
-  build: {},
-  server: {
-    allowedHosts: ['']
-  }
-})  
+  plugins: [react(), tailwindcss()],
+});
